08f16a730f254921ebdf14fc810f23bc2416a3fc
1h_mmm <yoltemur@gmail.com>
2020-12-26 15:38:27 -0600

    styles added to posts

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -2,17 +2,22 @@ import React from 'react';
 import {useSelector} from 'react-redux';
 import Post from './Post/Post';
 import useStyles from './styles.js';
+import {Grid, CircularProgress} from '@material-ui/core';
 
 function Posts() {
     const classes = useStyles();
     const posts = useSelector((state)=>state.posts)
     console.log(posts)
     return (
-        <>
-            <h1>Posta</h1>
-            <Post/>
-            <Post/>
-        </>
+        !posts.length ? <CircularProgress /> : (
+            <Grid className='classes.container' container alignItems='stretch' spacing={3}>
+                {posts.map((post)=>{
+                    <Grid item key={post.id} xs={12} sm={6}>
+                        <Post post={post} />
+                    </Grid>
+                })}
+            </Grid>
+        )
     )
 }
 

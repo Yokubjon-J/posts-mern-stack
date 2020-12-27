0e895c98e1ee8774b4a9049c56ad7a65783d6b9b
1h_mmm <yoltemur@gmail.com>
2020-12-27 15:22:28 -0600

    'final!'

--- server/index.js
+++ server/index.js
@@ -13,6 +13,10 @@ app.use(bodyParser.urlencoded({limit: '35mb', extended: true}));
 app.use(cors());
 app.use('/posts', postRoutes);
 
+app.get('/', (req,res)=>{
+        res.send('this is a backyard');
+})
+
 const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.URL_CONNECTION, {useNewUrlParser:true, useUnifiedTopology:true})
         .then(()=>app.listen(PORT, ()=>console.log(`Server listening on PORT ${PORT}`)))
